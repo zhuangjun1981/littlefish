2b0571b53be29e195a408ca8cbc8d56363774040
zhuangjun1981 <zhuangjun1981@gmail.com>
2017-06-28 22:05:21 -0700

    tiny change

--- littlefish/scripts/simulation_for_real/002_run_simulation_population_multi_thread.py
+++ littlefish/scripts/simulation_for_real/002_run_simulation_population_multi_thread.py
@@ -6,7 +6,6 @@ from multiprocessing import Pool
 import littlefish.core.utilities as util
 import littlefish.core.simulation as si
 
-
 SIMULATION_LENGTH = 50000
 SIMULATION_NUM = 3
 TERRAIN_SIZE = [128, 128]
@@ -15,7 +14,8 @@ FOOD_NUM = 200
 HARD_THR = 1000
 
 
-def simulation_fish_multiprocessing(f_path):
+def simulation_fish_multiprocessing(simulation_params):
+    f_path, fish_ind, fish_num = simulation_params
     si.simulate_one_fish(fish_path=f_path,
                          simulation_length=SIMULATION_LENGTH,
                          simulation_num=SIMULATION_NUM,
@@ -23,14 +23,14 @@ def simulation_fish_multiprocessing(f_path):
                          sea_level=SEA_LEVEL,
                          food_num=FOOD_NUM,
                          hard_thr=HARD_THR,
-                         fish_ind=0,
-                         fish_num=0)
+                         fish_ind=fish_ind,
+                         fish_num=fish_num)
 
 
 if __name__ == '__main__':
 
     data_folder = r"C:\little_fish_simulation_logs"
-    generation_num = 1
+    generation_num = 60
     process_num = 6
 
     gen_folder = os.path.join(data_folder, 'generation_' + util.int2str(generation_num, 6))
@@ -40,10 +40,12 @@ if __name__ == '__main__':
     fish_lst.sort()
     print('\n'.join(fish_lst) + '\n')
 
+    sim_params = zip(fish_lst, range(len(fish_lst)), [len(fish_lst)] * len(fish_lst))
+
     t0 = time.time()
 
     with Pool(5) as p:
-        p.map(simulation_fish_multiprocessing, fish_lst)
+        p.map(simulation_fish_multiprocessing, sim_params)
 
     print("time: {} min.".format((time.time() - t0) / 60))
 
