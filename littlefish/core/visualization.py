fe49e3e0fa7880bfdc2453b7d62919485eb1a5ed
zhuangjun1981 <zhuangjun1981@gmail.com>
2017-07-05 18:02:58 -0700

    plot brain finished.

---
+++ littlefish/core/visualization.py
@@ -0,0 +1,103 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import littlefish.core.utilities as util
+
+
+def plot_brain(input_brain, plot_axis=None, cmap='jet', bl_range=(-0.1, 0.1), ca_lw_cap=4., ca_range=(-1., 1.),
+               neuron_kws=None, connection_kws=None, layer_label_kws=None, neuron_label_kws=None):
+    """
+    plot the neurons and connections of a given brain, neuron color indicates its baseline firing rate, connection
+    color indicates connection amplitude
+
+    :param input_brain: littlefish.fish.Brain object
+    :param plot_axis: matplotlib.Axes object
+    :param cmap: matplotlib.cm object
+    :param bl_range: tuple of two floats, plotting range of neuron baseline firing rate, to be mapped to the cmap
+    :param ca_lw_cap: positive float, cap line width to indicating connection amplitude
+    :param ca_range: tuple of two floats, plotting range of connection amplitude, to be mapped to the cmap and
+                     line width
+    :param neuron_kws: dict, keyword arguments to plot neuron, matplotlib.Axis.plot inputs
+    :param connection_kws: dict, keyword arguments to plot connection, matplotl.Axis.plot inputs
+    :param layer_label_kws: dict, keyword arguments, to print text
+    :return:
+    """
+
+    if neuron_kws is None:
+        neuron_kws = {'mew': 4, 'ms': 80.}
+
+    if layer_label_kws is None:
+        layer_label_kws = {'fontsize': 15, 'va': 'center', 'ha': 'center', 'color': '#333333',
+                           'weight': 'semibold'}
+
+    if neuron_label_kws is None:
+        neuron_label_kws = {'fontsize': 15, 'va': 'center', 'ha': 'center', 'color': '#dddddd',
+                            'family': 'monospace', 'weight': 'semibold'}
+
+    if connection_kws is None:
+        connection_kws = {}
+
+    if plot_axis is None:
+        f = plt.figure(figsize=(10, 8), facecolor='#aaaaaa')
+        plot_axis = f.add_axes([0., 0., 1., 1.])
+
+    layer_num = input_brain.layer_num
+    layer_x_pos = np.linspace(0.1, 0.9, layer_num, endpoint=True)
+    neurons_df = input_brain.get_neurons().copy()
+
+    neuron_y_pos = []
+    for layer_ind in range(layer_num):
+        neuron_num = len(neurons_df[neurons_df['layer'] == float(layer_ind)])
+        neuron_y_pos.append(np.linspace(0.85, 0.1, neuron_num, endpoint=True))
+
+    xy_pos = [[[layer_x_pos[l], neuron_y_pos[l][n]] for n in range(len(neuron_y_pos[l]))] for l in range(layer_num)]
+    xy_pos = np.concatenate([np.array(p) for p in xy_pos], axis=0)
+    neurons_df['plot_y'] = xy_pos[:, 1]
+    neurons_df['plot_x'] = xy_pos[:, 0]
+
+    # print layer type
+    for layer in range(layer_num):
+        layer_type = input_brain.get_layer_type(layer)
+        plot_axis.text(layer_x_pos[layer], 0.92, layer_type.upper(), **layer_label_kws)
+
+    # plot connections
+    for gap, gap_conn in input_brain._connections.items():
+        for i in range(len(gap_conn.index)):
+            for j in range(len(gap_conn.columns)):
+                pre_ind = gap_conn.columns[j]
+                post_ind = gap_conn.index[i]
+                curr_conn = gap_conn.iloc[i, j]
+                pre_x = neurons_df.loc[pre_ind, 'plot_x']
+                pre_y = neurons_df.loc[pre_ind, 'plot_y']
+                post_x = neurons_df.loc[post_ind, 'plot_x']
+                post_y = neurons_df.loc[post_ind, 'plot_y']
+                amp_raw = curr_conn.get_amplitude()
+                amp_n = util.normalized_to_range(var=amp_raw, input_range=ca_range, output_range=(0., 1.))
+                amp_c = util.value_2_rgb(value=amp_n, cmap=cmap)
+                amp_lw = util.normalized_to_range(var=abs(amp_raw), input_range=[0, max([abs(c) for c in ca_range])],
+                                                  output_range=[0., ca_lw_cap])
+                plot_axis.plot([pre_x, post_x], [pre_y, post_y], color=amp_c, lw=amp_lw, **connection_kws)
+
+    # plotting neurons
+    for neuron_ind, neuron in neurons_df.iterrows():
+        bl = util.normalized_to_range(var=neuron['neuron'].get_baseline_rate(),
+                                      input_range=bl_range, output_range=(0., 1.))
+        bl_c = util.value_2_rgb(value=bl, cmap=cmap)
+        if neuron['neuron'].get_neuron_type() == 'neuron':
+            plot_axis.plot(neuron['plot_x'], neuron['plot_y'], '.', mfc=bl_c, mec='#222222', **neuron_kws)
+        elif neuron['neuron'].get_neuron_type() == 'muscle':
+            plot_axis.plot(neuron['plot_x'], neuron['plot_y'], '.', mfc=bl_c, mec='#222222', **neuron_kws)
+            plot_axis.text(neuron['plot_x'], neuron['plot_y'],
+                           util.short(neuron['neuron'].get_direction()), **neuron_label_kws)
+        elif neuron['neuron'].get_neuron_type() == 'eye':
+            if neuron['neuron'].get_input_type() == 'terrain':
+                plot_axis.plot(neuron['plot_x'], neuron['plot_y'], '.', mfc=bl_c, mec='#006622', **neuron_kws)
+            elif neuron['neuron'].get_input_type() == 'food':
+                plot_axis.plot(neuron['plot_x'], neuron['plot_y'], '.', mfc=bl_c, mec='#660000', **neuron_kws)
+            plot_axis.text(neuron['plot_x'], neuron['plot_y'],
+                           util.short(neuron['neuron'].get_direction()), **neuron_label_kws)
+
+    plot_axis.set_axis_off()
+    plot_axis.set_xlim([0, 1])
+    plot_axis.set_ylim([0, 1])
+    plt.show()
+
--- littlefish/test/test_utilities.py
+++ littlefish/test/test_utilities.py
@@ -3,7 +3,6 @@
 # from builtins import *
 
 import unittest
-
 import littlefish.core.utilities as util
 import numpy as np
 
@@ -15,13 +14,48 @@ class TestUtilities(unittest.TestCase):
 
     def test_array_nor(self):
         arr = np.array([1, 2, 3])
-        assert(np.array_equal(util.array_nor(arr), np.array([0, 0.5, 1])))
+        assert (np.array_equal(util.array_nor(arr), np.array([0, 0.5, 1])))
 
     def test_discreat_crosscorrelation(self):
         ts_trigger = np.arange(5)
         ts_reference = np.arange(5) + 0.1001
         ccg, t = util.discreat_crosscorrelation(ts_trigger, ts_reference, t_range=(-0.2, 0.5), bin_width=0.1)
-        assert(np.array_equal(ccg, [0, 0, 0, 5, 0, 0, 0]))
+        assert (np.array_equal(ccg, [0, 0, 0, 5, 0, 0, 0]))
+
+    def test_get_color_str(self):
+        assert (util.get_color_str(0, 0, 0) == '#000000')
+        assert (util.get_color_str(255, 0, 0) == '#ff0000')
+        assert (util.get_color_str(0, 255, 0) == '#00ff00')
+        assert (util.get_color_str(0, 0, 255) == '#0000ff')
+
+    def test_normalized_to_range(self):
+        assert (round(util.normalized_to_range(var=0.5, input_range=(0., 1.), output_range=(0., 2.)), 10) == 1.)
+        assert (round(util.normalized_to_range(var=0.8, input_range=(0., 1.), output_range=(0., 2.)), 10) == 1.6)
+        assert (round(util.normalized_to_range(var=0.8, input_range=(0., 1.), output_range=(1., 4.)), 10) == 3.4)
+
+    def test_value_2_rgb(self):
+        import matplotlib.pyplot as plt
+        import matplotlib
+        plt.ioff()
+        cmap = 'RdBu_r'
+        norm = matplotlib.colors.Normalize(vmin=0., vmax=99.)
+        f = plt.figure(figsize=(8, 4))
+        ax1 = f.add_subplot(121)
+        ax2 = f.add_subplot(122)
+        plot_arr = np.arange(100).reshape((10, 10))
+        ax1.imshow(plot_arr, vmin=0., vmax=99., cmap=cmap, interpolation='nearest')
+        for i in range(10):
+            for j in range(10):
+                # curr_c = util.normalized_to_range(plot_arr[i, j], (0., 99.), (0., 1.))
+                curr_c = norm(plot_arr[i, j])
+                curr_cstr = util.value_2_rgb(curr_c, cmap)
+                ax2.plot(j, i, 'o', mfc=curr_cstr, lw=0, mec='none', ms=20)
+        ax2.set_xlim([-0.5, 9.5])
+        ax2.set_ylim([9.5, -0.5])
+        plt.show()
+
+
+
 
 
 if __name__ == '__main__':
