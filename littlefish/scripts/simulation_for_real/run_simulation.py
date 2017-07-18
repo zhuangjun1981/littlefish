e0b7d8ba93f2b0d15a5be94755d8a206a48b3a52
zhuangjun1981 <zhuangjun1981@gmail.com>
2017-07-17 18:51:01 -0700

    small change

--- littlefish/scripts/simulation_for_real/run_simulation.py
+++ littlefish/scripts/simulation_for_real/run_simulation.py
@@ -4,6 +4,9 @@ import littlefish.core.evolution as evo
 
 def run():
 
+    start_generation_ind = 0
+    end_generation_ind = 5
+
     random_seed = random.randrange(2 ** 32 - 1)
     random.seed(random_seed)
     np.random.seed(random_seed)
@@ -24,10 +27,10 @@ def run():
     pe = evo.PopulationEvolution(base_folder=data_folder, generation_digits_num=7)
     brain_mutation = evo.get_default_brain_mutation()
 
-    pe.run(start_generation_ind=0, end_generation_ind=5, brain_mutation=brain_mutation,
-           population_size=population_size, process_num=process_num, turnover_rate=turnover_rate,
-           simulation_length=simulation_length, terrain_size=terrain_size, sea_portion=sea_portion,
-           terrain_filter_sigma=terrain_filter_sigma, food_num=food_num,
+    pe.run(start_generation_ind=start_generation_ind, end_generation_ind=end_generation_ind,
+           brain_mutation=brain_mutation, population_size=population_size, process_num=process_num,
+           turnover_rate=turnover_rate, simulation_length=simulation_length, terrain_size=terrain_size,
+           sea_portion=sea_portion, terrain_filter_sigma=terrain_filter_sigma, food_num=food_num,
            neuron_mutation_rate=neuron_mutation_rate, connection_mutation_rate=connection_mutation_rate)
 
 
