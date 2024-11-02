413cd122c141b30e70ae54b17797d0f7a1cbbce7
Jun Zhuang <wood_stocker@hotmail.com>
2024-11-02 14:54:05 -0700

    Small fix

--- littlefish/scripts/for_test/04_test_simulation_real.py
+++ littlefish/scripts/for_test/04_test_simulation_real.py
@@ -12,15 +12,16 @@ random.seed(111)
 np.random.seed(50)
 
 tg = tr.TerrainGenerator(size=[128, 128], sea_level=0.6)
-terrain_map = tg.generate_binary_map(sigma=3., is_plot=True)
+terrain_map = tg.generate_binary_map(sigma=3.0, is_plot=True)
 plt.show()
 terrain = tr.BinaryTerrain(terrain_map)
 fish = fi.generate_standard_fish()
 
-simulation = si.Simulation(terrain=terrain, fish_list=[fish],
-                           simulation_length=simulation_length, food_num=20)
+simulation = si.Simulation(
+    terrain=terrain, fish_list=[fish], simulation_length=simulation_length, food_num=20
+)
 
 simulation.initiate_simulation()
 simulation.run(verbose=1)
 
-print 'for debug ...'
+print("for debug ...")
